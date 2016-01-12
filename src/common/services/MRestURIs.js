a52dadbf150ce9c1742f7a78ca051b868f423f7c
schitupolu <schitupolu@FGT-schitupolu.fortinet-us.com>
2016-01-12 13:38:53 -0800

    Changed SEARCH_TWEETS URL to point to Google App Engine Application. Added Files under "bin" and "build".

--- src/common/services/MRestURIs.js
+++ src/common/services/MRestURIs.js
@@ -1,5 +1,7 @@
 angular.module('myapp.mresturis', [])
     .constant('M_RestURIsService', {
         //Tweets
-        SEARCH_TWEETS: 'http://localhost:8080/Twitter/search/username/'
+        //SEARCH_TWEETS: 'http://localhost:8080/Twitter/search/username/'
+        SEARCH_TWEETS: 'http://gae-twitter-app.appspot.com/rest/twitter/search/username/'
+
     });
